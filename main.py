710c47893739671e37b21a8086797878ffb592cf
yashwanthA555 <atlayashwanth32@gmail.com>
2026-02-05 16:03:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+# Input schema
+class QueryRequest(BaseModel):
+    question: str
 
 @app.get("/")
 def home():
     return {"message": "DocMind API is running"}
+
+@app.post("/chat")
+def chat(request: QueryRequest):
+    return {"you_asked": request.question}
